9b296c554e9eb01ff1e4df5a737cd83242f0bb06
Nils Hartmann <nils@nilshartmann.net>
2024-11-10 20:29:52 +0100

    Prepare BookList

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { IBookAbstract } from "./book-abstract-types.ts";
 import BookmarkButton from "./BookmarkButton.tsx";
 import { useRenderedLog } from "./use-rendered-log.ts";
+import { Link } from "@tanstack/react-router";
 
 type BookListProps = {
   books: IBookAbstract[];
@@ -22,13 +23,15 @@ export function BookList({ books }: BookListProps) {
               - make sure, bookId has correct type! (String!)
 
               */}
-            <h2
-              className={
-                "font-anton text-2xl decoration-2 underline-offset-4 hover:text-fuchsia-700 hover:underline"
-              }
-            >
-              {b.title}
-            </h2>
+            <Link>
+              <h2
+                className={
+                  "cursor-pointer font-anton text-2xl decoration-2 underline-offset-4 hover:text-fuchsia-700 hover:underline"
+                }
+              >
+                {b.title}
+              </h2>
+            </Link>
             <p>{b.likes} Likes</p>
             <BookmarkButton bookId={b.id} />
           </div>
