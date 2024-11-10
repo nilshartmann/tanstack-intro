edc938e6d561e2bad083ffe30a6ef80865762630
Nils Hartmann <nils@nilshartmann.net>
2024-11-10 20:40:45 +0100

    import

--- frontend/schritte/45_before_load/00_material/routes/books/$bookId/route.tsx
+++ frontend/schritte/45_before_load/00_material/routes/books/$bookId/route.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import Pending from "../../../components/Pending.tsx";
 import { addLogEvent } from "../../../event-store.ts";
+import { bookByIdQueryOpts } from "../../../components/book-query.ts";
+import { reviewsForBookQueryOpts } from "../../../components/reviews-query.ts";
 
 export const Route = createFileRoute("/books/$bookId")({
   beforeLoad({ params, context }) {
