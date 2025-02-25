d13e656bfa02ae22ab95dfc8ad69553430b104ba
Nils Hartmann <nils@nilshartmann.net>
2025-02-25 12:08:42 +0100

    schritte entfernt

--- frontend/schritte/00_initial/src/components/review-types.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const ReviewSchema = z.object({
-  id: z.number(),
-  reviewer: z.string(),
-  comment: z.string(),
-});
-
-export const GetReviewsResponseSchema = ReviewSchema.array();
--- frontend/schritte/README_LIVE_CODING.md
+++
@@ -1,2 +0,0 @@
-- In WebStorm die Live Template Gruppe "Live Coding TanStack Book Example" einschalten!
-- 
